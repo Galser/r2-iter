e4acd6ac1961d91490e0eef83da6b83fb7707ecd
galser <guseletov@gmail.com>
2025-09-15 16:40:00 +0200

    Shorten with iter_mut

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn print_elements(elements:  &[String]) {
     // }
     elements
         .iter()
-        .map(|el| format!("{} {} ", el, el))
+//        .map(|el| format!("{} {} ", el, el))
         .for_each(|el| println!("{}", el));
 }
 
 fn shorten_strings(elements:  &mut Vec<String>) {
-    elements.iter().for_each(|el| el.truncate(el.len()-1));
+    elements.iter_mut().for_each(|el| el.truncate(1));
 }
 
 fn main() {
@@ -26,6 +26,8 @@ fn main() {
     print_elements(&colors[1..3]);
 
     shorten_strings(&mut colors);
+
+    print_elements(&colors);
     // let mut colors_iter = colors.iter();
         
     // println!("{:#?}", colors_iter.next());
