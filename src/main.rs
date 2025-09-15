e6c0e069895f83bc3efc21b7a8d5eb75bbdd4f94
galser <guseletov@gmail.com>
2025-09-15 14:57:06 +0200

    Iterators init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    
+    let colors = vec![
+        String::from("red"),
+        String::from("green"),
+        String::from("blue"),
+        String::from("yellow"),
+    ]
+    
+    let mut colors_iter = colors.iter();
+    
+    println!("{:#?}}", colors_iter.next());
+    println!("{:#?}}", colors_iter.next());
+    println!("{:#?}}", colors_iter.next());
+    println!("{:#?}}", colors_iter.next());
+    println!("{:#?}}", colors_iter.next());
+}
