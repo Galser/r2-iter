d70ee4a2de24e5d0f0d9718825de4ddd2d872dd2
galser <guseletov@gmail.com>
2025-09-16 15:59:50 +0200

    Exploded function

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,28 @@ fn print_elements(elements:  &[String]) {
         .for_each(|el| println!("{}", el));
 }
 
-fn shorten_strings(elements:  &mut Vec<String>) {
+fn shorten_strings(elements:  &mut [String]) {
     elements.iter_mut().for_each(|el| el.truncate(1));
+}
+
+fn to_uppercase(elements:  &[String]) -> Vec<String> {
+    elements
+        .iter()
+        .map( |el| el.to_uppercase())
+        .collect()
+}
+
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|el| vec_b.push(el));
+}
+
+fn explode( elements:  &[String] ) -> Vec<Vec<String>> {
+    elements
+        .iter()
+        .map(
+            |el| el.chars().map(|c| c.to_string()).collect()
+        )
+        .collect()
 }
 
 fn main() {
@@ -23,16 +43,25 @@ fn main() {
         String::from("yellow"),
     ];
     
-    print_elements(&colors[1..3]);
-
-    shorten_strings(&mut colors);
-
     print_elements(&colors);
+
+    //shorten_strings(&mut colors[1..3]);
+    
+    // let uppercased = to_uppercase(&colors);
+
+    // print_elements(&uppercased);
     // let mut colors_iter = colors.iter();
         
     // println!("{:#?}", colors_iter.next());
+
     // println!("{:#?}", colors_iter.next());
     // println!("{:#?}", colors_iter.next());
     // println!("{:#?}", colors_iter.next());
     // println!("{:#?}", colors_iter.next());
+
+    // let mut destination = vec![];
+    // move_elements(colors, &mut destination);
+    // println!("Destination: {:#?}", destination);
+    let exploded = explode(&colors);
+    println!("{:#?}", exploded)
 }
